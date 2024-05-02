975b26f3ef02f8892f943b949d9a31c4125eec62
Santhosh <shanmugamsanthosh22@gmail.com>
2024-05-02 21:26:13 +0530

    nvhvbvg

--- config/supabase.js
+++ config/supabase.js
@@ -1,7 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
-import dotenv from "dotenv";
-dotenv.config();
 
-const supabase = createClient(process.env.DB_URL, process.env.DB_SECRET);
+const supabase = createClient(
+  "https://tuozwtvqglebbaueohaw.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR1b3p3dHZxZ2xlYmJhdWVvaGF3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTA4NTY2MjQsImV4cCI6MjAyNjQzMjYyNH0.CfSijV94B0COBgoeiEyXUEvSDLA4txqjwAvR4-LT7u0"
+);
 
 export default supabase;
